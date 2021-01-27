2476105dd1b237cc72e914dce937ae1b6ee5a2cc
Farrukh Atabekov <augini18@gmail.com>
2021-01-27 22:04:59 +0900

    feature: improve on client side

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -23,24 +23,51 @@ const AddRestaurant = () => {
     // }
   };
   return (
-    <div class="row">
-      <div class="col">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="First name"
-          aria-label="First name"
-        />
+    <form action="">
+      <div className="row">
+        <div className="col">
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            className="form-control"
+            placeholder="Name"
+          />
+        </div>
+        <div className="col">
+          <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            className="form-control"
+            type="text"
+            placeholder="Location"
+          />
+        </div>
+        <div className="col">
+          <select
+            value={priceRange}
+            onChange={(e) => setPriceRange(e.target.value)}
+            className="form-select custom-select my-1 mr-sm-2"
+          >
+            <option disabled>Price Range</option>
+            <option value="1">$</option>
+            <option value="2">$$</option>
+            <option value="3">$$$</option>
+            <option value="4">$$$$</option>
+            <option value="5">$$$$$</option>
+          </select>
+        </div>
+        <div className="col">
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      <div class="col">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="Last name"
-          aria-label="Last name"
-        />
-      </div>
-    </div>
+    </form>
   );
 };
 
